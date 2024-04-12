a4cdd27bfd97741d06239190209cccd71a758bef
Narutchai <koonfluk2015@gmail.com>
2024-04-13 00:39:47 +0700

    update

--- app/main.py
+++ app/main.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI
 from app.root import read_root
 from pydantic import BaseModel
+from ultralytics import YOLO
+import numpy as np
+import os
 
 app = FastAPI()
 
 
-class Data(BaseModel):
-    arrIMG: list[str] = []
-
+# class Data(BaseModel):
+#     arrIMG: list[str] = []
 
 
 @app.get("/")
@@ -16,5 +18,24 @@ def read_main():
 
 
 @app.post("/predict")
-async def predict(data : Data):
-    return {"data": data.arrIMG}
+async def predict():
+    HOME = os.getcwd()
+    modelPath = "./app/Model/best.pt"
+    model = YOLO(modelPath)
+
+    imageArr = [
+        "./app/testIMG/0cashew_valid_anthracnose.JPG",
+        "./app/testIMG/1cashew_valid_anthracnose.JPG",
+        "./app/testIMG/2cashew_valid_anthracnose.JPG",
+        "./app/testIMG/34cashew_valid_red rust.JPG",
+    ]
+
+    results = model("./app/testIMG/0cashew_valid_anthracnose.JPG")
+
+    names_dict = results[0].names
+
+    probs = results[0].probs.data.tolist()
+
+    return {
+    "predict": names_dict[np.argmax(probs)]
+    }
